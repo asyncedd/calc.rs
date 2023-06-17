e8f87d7442b3ed7af74d1d16f2fe874e6ddbb37a
asyncedd <isynqquwu@proton.me>
2023-06-17 10:47:31 +0900

    refactor(operators): modulalize that aswell

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,11 @@
 mod input;
+mod operators;
+
 use input::read_input;
-use lazy_static::lazy_static;
-use std::collections::HashSet;
+use operators::*;
 
 fn parse_f64(string: &str) -> f64 {
     string.parse().expect("Invalid number")
-}
-
-const VALID_OPERATORS: &[&str; 26] = &[
-    "+", "-", "*", "/", "^", "sqrt", "sine", "cosine", "tangent", "abs", "floor", "ceiling", "tan",
-    "asin", "acos", "ln", "log", "e ^", "sinh", "cosh", "tanh", "atan2", "atan", "primes",
-    "collatz", "perfect",
-];
-
-fn join_valid_operators() -> String {
-    VALID_OPERATORS.join(", ")
-}
-
-lazy_static! {
-    static ref VALID_OPERATORS_SET: HashSet<&'static str> = {
-        let set: HashSet<_> = VALID_OPERATORS.iter().cloned().collect();
-        set
-    };
-    static ref SINGLE_OPERATORS_SET: HashSet<&'static str> = {
-        let set: HashSet<_> = SINGLE_OPERATORS.iter().cloned().collect();
-        set
-    };
-    static ref GEN_OPERATORS_SET: HashSet<&'static str> = {
-        let set: HashSet<_> = GEN_OPERATORS.iter().cloned().collect();
-        set
-    };
-}
-
-fn is_valid_operator(operator: &str) -> bool {
-    VALID_OPERATORS_SET.contains(operator)
-}
-
-const SINGLE_OPERATORS: &[&str; 18] = &[
-    "sqrt", "sine", "cosine", "tangent", "abs", "floor", "ceiling", "tan", "asin", "acos", "ln",
-    "e ^", "sinh", "cosh", "tanh", "atan", "collatz", "perfect",
-];
-
-const GEN_OPERATORS: &[&str; 3] = &["primes", "collatz", "perfect"];
-
-fn check_if_certain_operator(operator: &str) -> bool {
-    SINGLE_OPERATORS_SET.contains(operator)
-}
-
-fn check_if_gen_operator(operator: &str) -> bool {
-    GEN_OPERATORS_SET.contains(operator)
 }
 
 fn get_second_thing(operator: &str) -> f64 {
---
+++ src/operators.rs
@@ -0,0 +1,47 @@
+// operators.rs
+use lazy_static::lazy_static;
+use std::collections::HashSet;
+
+const VALID_OPERATORS: &[&str; 26] = &[
+    "+", "-", "*", "/", "^", "sqrt", "sine", "cosine", "tangent", "abs", "floor", "ceiling", "tan",
+    "asin", "acos", "ln", "log", "e ^", "sinh", "cosh", "tanh", "atan2", "atan", "primes",
+    "collatz", "perfect",
+];
+
+const SINGLE_OPERATORS: &[&str; 18] = &[
+    "sqrt", "sine", "cosine", "tangent", "abs", "floor", "ceiling", "tan", "asin", "acos", "ln",
+    "e ^", "sinh", "cosh", "tanh", "atan", "collatz", "perfect",
+];
+
+const GEN_OPERATORS: &[&str; 3] = &["primes", "collatz", "perfect"];
+
+lazy_static! {
+    pub static ref VALID_OPERATORS_SET: HashSet<&'static str> = {
+        let set: HashSet<_> = VALID_OPERATORS.iter().cloned().collect();
+        set
+    };
+    pub static ref SINGLE_OPERATORS_SET: HashSet<&'static str> = {
+        let set: HashSet<_> = SINGLE_OPERATORS.iter().cloned().collect();
+        set
+    };
+    pub static ref GEN_OPERATORS_SET: HashSet<&'static str> = {
+        let set: HashSet<_> = GEN_OPERATORS.iter().cloned().collect();
+        set
+    };
+}
+
+pub fn join_valid_operators() -> String {
+    VALID_OPERATORS.join(", ")
+}
+
+pub fn is_valid_operator(operator: &str) -> bool {
+    VALID_OPERATORS_SET.contains(operator)
+}
+
+pub fn check_if_certain_operator(operator: &str) -> bool {
+    SINGLE_OPERATORS_SET.contains(operator)
+}
+
+pub fn check_if_gen_operator(operator: &str) -> bool {
+    GEN_OPERATORS_SET.contains(operator)
+}
