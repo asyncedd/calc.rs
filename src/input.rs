e63385b8fab272ebcf8673445df24b5b8719aac1
asyncedd <isynqquwu@proton.me>
2023-06-17 11:16:26 +0900

    feat(printing): improve the UI

--- src/input.rs
+++ src/input.rs
@@ -1,10 +1,14 @@
 // input.rs
 use std::io;
+use std::io::Write;
 
 pub fn read_input(message: &str) -> String {
     if message != "" {
         println!("{}", message);
     }
+    print!("> ");
+    io::stdout().flush().unwrap();
+
     let mut input = String::new();
     io::stdin()
         .read_line(&mut input)
